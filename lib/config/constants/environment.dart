ce7cdf131951e9f4b24127525109e875a2d56e93
Luis Angel Ibañez Barbosa <chainblack9@gmail.com>
2024-12-13 13:26:53 -0500

    Inject env with dart-define

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class Environment {
-  static String theMovieDBKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No Api Key';
+  static String theMovieDBKey = const String.fromEnvironment('THE_MOVIEDB_KEY');
 }
